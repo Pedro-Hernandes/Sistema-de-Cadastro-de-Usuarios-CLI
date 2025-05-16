5359f32e1ec41ba5d5c9f2ba51d65e0614434265
Pedro-Hernandes <106574915+Pedro-Hernandes@users.noreply.github.com>
2025-05-16 11:55:39 -0300

    Update User.java

--- User.java
+++ User.java
@@ -1,1 +1,54 @@
+package usermanagement;
 
+public class User {
+	
+	private int id;
+	private String name;
+	private String email;
+	
+	/**
+	 * Modelo de Usuario
+	*/
+	
+	
+	public User() {}
+	
+	public User(int id, String name, String email) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		
+	}
+
+	// Getters e Setters
+	
+	public int getId() {
+		this.id = id;
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void SetName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+    @Override
+    public String toString() {
+        return String.format("ID: %d | Nome: %s | Email: %s", id, name, email);
+    }
+}
